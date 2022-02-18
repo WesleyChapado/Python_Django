2b2f28a36f20497fd26b5a25822688121598daac
WesleyChapado <73513545+WesleyChapado@users.noreply.github.com>
2022-02-18 16:25:43 -0300

    Aula 5

--- alurareceita/settings.py
+++ alurareceita/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'alurareceita.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'alura_receita',
+        'USER': 'postgres',
+        'PASSWORD': '102030',
+        'HOST': 'localhost'
     }
 }
 
@@ -123,4 +126,3 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'alurareceita/static')
 ]
-
